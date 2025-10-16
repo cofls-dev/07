29ec1b83aa2ce384285c0aca085789cd05d00802
cofls-dev <cofls@sookmyung.ac.kr>
2025-10-16 15:37:30 +0900

    03

--- main.c
+++ main.c
@@ -6,19 +6,21 @@
 int main(int argc, char *argv[]) 
 {
 	
-	int i;
-
+	int i = 10;
+	printf ("함수 호출전 i=%d\n", i);
 	
-	
-	for (i=0; i<5 ; i++)
-	{
-		int temp = 1;
-		printf ("temp = %d\n", temp);
-		temp++;
-		
-	}
-
+	i=inc(i);
+	printf("함수 호출 후 i=%d\n", i);
 	
 	system ("PAUSE");
 	return 0;
+	
+}
+
+int inc(int counter)
+
+{
+	counter ++ ;
+	return counter;
+	
 }
