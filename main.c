b5a5647e8a1a19b6401ede8847cc31d1172c5db9
cofls-dev <cofls@sookmyung.ac.kr>
2025-10-16 15:49:30 +0900

    04

--- main.c
+++ main.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void f(void);
+
+
+int main(void)
 {
+	int i;
+	for (i=0; i<5; i++)
+	{
+		f();
+	}
 	
-	int i = 10;
-	printf ("함수 호출전 i=%d\n", i);
-	
-	i=inc(i);
-	printf("함수 호출 후 i=%d\n", i);
-	
-	system ("PAUSE");
+	system("PAUSE");
 	return 0;
-	
 }
 
-int inc(int counter)
-
+void f(void)
 {
-	counter ++ ;
-	return counter;
-	
+	int i;
+	for (i=0; i<10; i++)
+	printf("#");
+
 }
